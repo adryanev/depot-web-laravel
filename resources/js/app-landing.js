b2781af83a01dce832634a9039f5dc76d6e23557
adryanev <adryanekavandra@gmail.com>
2021-01-27 21:46:36 +0700

    Initial commit

---
+++ resources/js/app-landing.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+require('alpinejs');
+require('moment')
+require('owl.carousel')
+require('jquery-bar-rating')
+require('headroom')
+require('jquery.scrollto')
+require('bootstrap-notify')
+require('./dore.scripts.landingpage')
+require('./scripts.single.theme')
+require('./custom')
